c811eb2d18f42aca45c5994f183247d9d27ee859
welizaa <wmss6@aluno.ifal.edu.br>
2024-12-02 20:55:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+ let num1 = parseFloat(prompt('Digite o primeiro numero:'));
+    let num2 = parseFloat(prompt('Digite o segundo numero:'));
+    let num3 = parseFloat(prompt('Digite o terceiro numero:'));
+
+    
+    if (num1 >= num2 && num1 >= num3) {
+
+        alert('O maior numero e: ' + num1);
+
+    } else if (numero2 >= numero1 && numero2 >= numero3) {
+
+        alert('O maior numero e: ' + num2);
+
+    } else {
+
+        alert('O maior numero e: ' + num3);
+
+    }
